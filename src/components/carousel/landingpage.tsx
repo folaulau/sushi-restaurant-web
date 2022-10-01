eb975fffcdb01ac829cb4ded1293b472a8b567c1
folaulau <folaukaveinga@gmail.com>
2022-10-01 17:48:28 -0600

    updated landing page carosuel

--- src/components/carousel/landingpage.tsx
+++ src/components/carousel/landingpage.tsx
@@ -5,55 +5,43 @@ import rev2 from '../../images/landingpage/rev_img2.png';
 import rev3 from '../../images/landingpage/rev_img3.png';
 import { Link } from "react-router-dom";
 
+
+const slides = [
+    {
+        id: 1,
+        img: rev1
+    },{
+        id: 2,
+        img: rev2
+    },{
+        id: 3,
+        img: rev3
+    },
+]
+
 function LandingPage_Top() {
 
     return (
         <Carousel variant="dark">
-            <Carousel.Item>
-                <div className='row'>
-                    <div className='col-6'>
-                        <img
-                            className="landing-carousel"
-                            src={rev1}
-                            alt="First slide"
-                        />
-                    </div>  
-                    <div className='col-6 landing-carousel-title'>
-                        <h3>True Taste of Sushi</h3>
-                        <Link to={`/menu`} className="btn btn-outline-primary">Order Online</Link>
-                    </div>  
-                </div>
-            </Carousel.Item>
-            <Carousel.Item>
-                <div className='row'>
-                    <div className='col-6'>
-                        <img
-                            className="landing-carousel"
-                            src={rev2}
-                            alt="First slide"
-                        />
-                    </div>  
-                    <div className='col-6 landing-carousel-title'>
-                        <h3>True Taste of Sushi</h3>
-                        <button type="button" className="btn btn-outline-primary">Order Online</button>
-                    </div>  
-                </div>
-            </Carousel.Item>
-            <Carousel.Item>
-                <div className='row'>
-                    <div className='col-6'>
-                        <img
-                            className="landing-carousel"
-                            src={rev3}
-                            alt="First slide"
-                        />
-                    </div>  
-                    <div className='col-6 landing-carousel-title'>
-                        <h3>True Taste of Sushi</h3>
-                        <button type="button" className="btn btn-outline-primary">Order Online</button>
-                    </div>  
-                </div>
-            </Carousel.Item>
+            {
+                slides.map((slide)=>(
+                    <Carousel.Item key={slide.id}>
+                        <div className='row'>
+                            <div className='col-6'>
+                                <img
+                                    className="landing-carousel"
+                                    src={slide.img}
+                                    alt="First slide"
+                                />
+                            </div>  
+                            <div className='col-6 landing-carousel-title'>
+                                <h3>True Taste of Sushi</h3>
+                                <Link to={`/reservation`} className="btn btn-outline-primary me-2">Book a Table</Link> <Link to={`/menu`} className="btn btn-outline-primary">Order Online</Link>
+                            </div>  
+                        </div>
+                    </Carousel.Item>
+                ))
+            }
         </Carousel>
     );
   
