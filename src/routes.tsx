1d9cc738ff50774f8371e0496530bf082e899e1b
Folau Kaveinga <folaukaveinga@gmail.com>
2024-01-21 23:54:55 -0700

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState} from "react";
+import { useEffect} from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
